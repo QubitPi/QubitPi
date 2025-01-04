c4aaaec2fea3b603c0b74138d563dda1c5c9a0af
Jack <jack20220723@gmail.com>
2025-01-04 09:58:32 +0800

    Fix window bug

--- compute_active_forks.py
+++ compute_active_forks.py
@@ -4,13 +4,13 @@ import datetime
 MAX_NUM_ACTIVE_FORKS_TO_SHOW = 6
 ALL_MY_PRS = "https://api.github.com/search/issues?q=merged:>{merged_after} author:QubitPi type:pr"
 PIN_TEMPLATE = "[![{repo_name}](https://github-readme-stats.vercel.app/api/pin/?username={owner}&repo={repo_name}&show_owner=true&theme=ambient_gradient)](https://github.com/{owner}/{repo_name})"
-ACTIVE_WINDOW_IN_HOURS = 24
+ACTIVE_WINDOW_IN_DAY = 1
 
 def get_active_forks():
     today = datetime.date.today()
-    five_days_ago = today - datetime.timedelta(hours=ACTIVE_WINDOW_IN_HOURS)
+    retrospection_window = today - datetime.timedelta(hours=ACTIVE_WINDOW_IN_DAY)
 
-    prs = requests.get(url=ALL_MY_PRS.format(merged_after=five_days_ago)).json()["items"]
+    prs = requests.get(url=ALL_MY_PRS.format(merged_after=retrospection_window)).json()["items"]
 
     repository_urls = list(set([pr["repository_url"] for pr in prs]))
 
