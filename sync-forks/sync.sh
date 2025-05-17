adf095fccd595b3ae2e38c9131e7a3e5ae379ae8
Jack <jack20220723@gmail.com>
2025-05-17 10:50:39 +0800

    Update sync

--- sync-forks/sync.sh
+++ sync-forks/sync.sh
@@ -5,12 +5,12 @@ set -e
 python3 sync.py -f git@github.com:QubitPi/leadership-blogs.git -u git@github.com:saicaca/fuwari.git -ub main
 
 # ML
-python3 sync.py -f git@github.com:QubitPi/cpython.git -u git@github.com:python/cpython.git -ub main
 python3 sync.py -f git@github.com:QubitPi/mlflow.git -u git@github.com:mlflow/mlflow.git -ub master
-python3 sync.py -f git@github.com:QubitPi/nx-guides.git -u git@github.com:networkx/nx-guides.git -ub main
-python3 sync.py -f git@github.com:QubitPi/scikit-learn.git -u git@github.com:scikit-learn/scikit-learn.git -ub main
 python3 sync.py -f git@github.com:QubitPi/openai-spinningup.git -u org-14957082@github.com:openai/spinningup.git -ub master
+python3 sync.py -f git@github.com:QubitPi/scikit-learn.git -u git@github.com:scikit-learn/scikit-learn.git -ub main
+python3 sync.py -f git@github.com:QubitPi/nx-guides.git -u git@github.com:networkx/nx-guides.git -ub main
 python3 sync.py -f git@github.com:QubitPi/numpy.git -u git@github.com:numpy/numpy.git -ub main
+python3 sync.py -f git@github.com:QubitPi/cpython.git -u git@github.com:python/cpython.git -ub main
 
 # UI
 python3 sync.py -f git@github.com:QubitPi/react.dev.git -u git@github.com:reactjs/react.dev.git -ub main
