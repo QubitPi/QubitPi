b4fc821c1b67e63443debc4bc8be448d6ef59488
Jack <jack20220723@gmail.com>
2025-06-21 16:00:53 +0800

    Remove bad fork

--- sync-forks/sync.sh
+++ sync-forks/sync.sh
@@ -2,8 +2,6 @@
 set -x
 set -e
 
-python3 sync.py -f git@github.com:QubitPi/openml-docs.git -u git@github.com:openml/docs.git -ub master
-
 python3 sync.py -f git@github.com:QubitPi/leadership-blogs.git -u git@github.com:saicaca/fuwari.git -ub main
 python3 sync.py -f git@github.com:QubitPi/wg-easy.git -u git@github.com:wg-easy/wg-easy.git -ub master
 python3 sync.py -f git@github.com:QubitPi/mieru.git -u git@github.com:enfein/mieru.git -ub main
