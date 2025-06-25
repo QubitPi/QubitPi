97b72014acbd9237ae2a33457f693b386a39e3cb
Jack <jack20220723@gmail.com>
2025-06-25 16:35:31 +0800

    Add fork

--- sync-forks/sync.sh
+++ sync-forks/sync.sh
@@ -75,6 +75,7 @@ python3 sync.py -f git@github.com:QubitPi/fastapi.git                     -u git
 python3 sync.py -f git@github.com:QubitPi/jekyll-theme-jakarta-ee.git     -u git@github.com:jakartaee/jekyll-theme-jakarta-ee.git     -ub master
 python3 sync.py -f git@github.com:QubitPi/jakartaee-rest.git              -u git@github.com:jakartaee/rest.git                        -ub main
 python3 sync.py -f git@github.com:QubitPi/golang-website.git              -u git@github.com:golang/website.git                        -ub master
+python3 sync.py -f git@github.com:QubitPi/json-api.git                    -u git@github.com:json-api/json-api.git                     -ub gh-pages
 python3 sync.py -f git@github.com:QubitPi/graphql-spec.git                -u git@github.com:graphql/graphql-spec.git                  -ub main
 
 # HashiCorp
