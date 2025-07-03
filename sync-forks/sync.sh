db23fc63b38e73dff4302e5a1e0b596031e8d9f5
Jack <jack20220723@gmail.com>
2025-07-03 10:54:34 +0800

    New fork

--- sync-forks/sync.sh
+++ sync-forks/sync.sh
@@ -112,6 +112,7 @@ python3 sync.py -f git@github.com:QubitPi/writethedocs.git                     -
 python3 sync.py -f git@github.com:QubitPi/diataxis-documentation-framework.git -u git@github.com:evildmp/diataxis-documentation-framework.git -ub main
 python3 sync.py -f git@github.com:QubitPi/progit2.git                          -u git@github.com:progit/progit2.git                           -ub main
 python3 sync.py -f git@github.com:QubitPi/Bit-Twiddling-Hacks.git              -u git@github.com:virtyaluk/Bit-Twiddling-Hacks.git            -ub master
+python3 sync.py -f git@github.com:QubitPi/github-repo-pins.git                 -u git@github.com:anuraghazra/github-readme-stats.git          -ub master
 
 # Special case for https://github.com/QubitPi/glassfish-hk2
 # Because they put doc source code in gh-pages, a branch which is supposed to be automatically managed
